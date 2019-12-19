c141c28820bd54f6bfd5dceadfea4b5b38174e05
KatWee <katkat.w@hotmail.com>
2019-12-20 00:04:18 +0700

    fix <a> to <Link>

--- cpegames/src/App.js
+++ cpegames/src/App.js
@@ -1,6 +1,5 @@
 import React,{Component} from 'react';
-import Header from './Header.js'
-import Body from './Body.js'
+import {Link} from 'react-router-dom'
 class App extends Component{
   // constructor(){
   //   super();
@@ -45,9 +44,9 @@ class App extends Component{
   render (){
       return(
         <div>
-            <h1><a href="/header">Header</a></h1>
-            <h1><a href="/body">Body</a></h1>
-            <h1><a href="/">Home</a></h1>
+            <h1><Link to="/header">Header</Link></h1>
+            <h1><Link to="/body">Body</Link></h1>
+            <h1><Link to="/">Home</Link></h1>
         </div>
       );
   }
