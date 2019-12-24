b3677b2b98463eeffb2d625c5e9ba127f8c0e72f
KatWee <katkat.w@hotmail.com>
2019-12-24 15:58:45 +0700

    change card to tab

--- cpegames/src/Component/ScoreCard.js
+++ cpegames/src/Component/ScoreCard.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import { Card } from "antd";
-import CardCSS from "./Card.css"
-import { Tag } from 'antd';
+import CardCSS from "./Card.css";
+import { Tag } from "antd";
+import { Tabs } from "antd";
+
 class ScoreCard extends Component {
-    
   render() {
     const teamName = this.props.team.toUpperCase();
     const CardTitle = `${teamName} SCORE`;
-    const teamColor = this.props.team == "angel" ? "#467DFF" : "#FF6369"
+    const teamColor = this.props.team == "angel" ? "#467DFF" : "#FF6369";
+    const { TabPane } = Tabs;
     return (
       <div>
-        <Card title={CardTitle} bordered={false}>
-            <p style={{color:`${teamColor}`}}>100</p>
-        </Card>
+        <div className="card-container">
+          <Tabs type="card" >
+            <TabPane tab={CardTitle} key="1">
+              <p style={{ color: `${teamColor}` }}>100</p>
+            </TabPane>
+          </Tabs>
+        </div>
       </div>
     );
   }
